3f60fede35a0c7e9d3b6ffbb92ffc6bd99789437
diewme <129123887+17Andrey@users.noreply.github.com>
2025-05-09 02:57:29 +0500

    fit : auth

--- src/config.ts
+++
@@ -1,5 +0,0 @@
-const isDevelopment = process.env.NODE_ENV === 'development';
-
-export const API_URL = isDevelopment 
-  ? 'http://localhost:3000'
-  : 'https://your-api-domain.com'; // Замените на реальный домен вашего API 
